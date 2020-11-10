36788f910fe0c18ae8f63ff3fe1ac3212b7d1656
Icesnowliu1128 <3194529680@qq.com>
2020-11-10 17:30:55 +0800

    taobao2

---
+++ build/precache-manifest.05a0ffb4452aa6ab6f0e709ecac93f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7a16fc04b091503e370bd964a75a3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fe159c6711476201539",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "53b1a56743cbd78b8881",
+    "url": "./static/css/main.afb88211.chunk.css"
+  },
+  {
+    "revision": "7fe159c6711476201539",
+    "url": "./static/js/2.b2de083f.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "./static/js/2.b2de083f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b1a56743cbd78b8881",
+    "url": "./static/js/main.85682ad3.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
